746a4f266dd4b79b50f0c5b1ee42a8ca625ff904
Le Sy Dat <datls@sixthgearstudios.com>
2017-04-03 14:17:51 +0700

    Init first version

---
+++ README.markdown
@@ -0,0 +1,1 @@
+# Server version of Meeting Bullet
---
+++ application/logs/log-2017-04-03.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-04-03 09:07:46 --> 404 Page Not Found: install
