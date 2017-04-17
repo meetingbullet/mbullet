ba6154be5ccf57fb0b0653e13dae6780ec2aa00b
VietHD <viethd@sixthgearstudios.com>
2017-04-17 16:19:04 +0700

    Fix login page doesn't display form error & Add notify plugin into default theme

--- public/themes/default/footer.php
+++ public/themes/default/footer.php
@@ -19,6 +19,7 @@
 	<!--<script src="<?php echo Template::theme_url("js-plugins/Chart.min.js"); ?>" type="text/javascript"></script>-->
 	<!--<script src="<?php echo Template::theme_url("js-plugins/circle-progress.min.js"); ?>" type="text/javascript"></script>-->
 	<!--<script src="<?php echo Template::theme_url("js/customize-chart.js"); ?>" type="text/javascript"></script>-->
+	<script src="<?php echo Template::theme_url("js-plugins/bootstrap-notify.js"); ?>" type="text/javascript"></script>
 	<script src="<?php echo Template::theme_url("js/scripts.js"); ?>" type="text/javascript"></script>
 	<script src="<?php echo Template::theme_url("js/main.js"); ?>" type="text/javascript"></script>
 </body>
