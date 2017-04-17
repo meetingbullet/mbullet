1f00cb8ff94a5fff4c5365eb0283ba11f0eecd1d
VietHD <viethd@sixthgearstudios.com>
2017-04-17 16:50:21 +0700

    Update modal format

--- application/modules/projects/views/index.php
+++ application/modules/projects/views/index.php
@@ -2,7 +2,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="bigModal" tabindex="-1" role="dialog" aria-labelledby="bigModalLabel">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       
     </div>
