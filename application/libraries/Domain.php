9b30eca486f6b4242dcc028e9023b704cb67477f
Dinh Gia Bao <baodg@sixthgearstudios.com>
2017-04-18 13:53:26 +0700

    Update authenticated controller

---
+++ application/libraries/Domain.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+class Domain
+{
+	private $ci;
+	
+	public function __construct()
+	{
+		$this->ci =& get_instance();
+	}
+
+	public function get_main_domain()
+	{
+		$current_domain = $_SERVER['SERVER_NAME'];
+		$parsed_url = explode('.', $current_domain);
+		$main_domain_parts = [];
+		for ($i = (count($parsed_url) - MAIN_DOMAIN_PARTS); $i < count($parsed_url); $i++) {
+			$main_domain_parts[] = $parsed_url[$i];
+		}
+		$main_domain = implode('.', $main_domain_parts);
+
+		return $main_domain;
+	}
+}
