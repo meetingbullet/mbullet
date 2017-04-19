9f8e480ccce3924db3e2c8d2a53096fe7c73d998
Le Sy Dat <datls@sixthgearstudios.com>
2017-04-19 19:56:56 +0700

    remove sidebar

--- public/themes/user/header.php
+++ public/themes/user/header.php
@@ -1,6 +1,6 @@
 <?php
 
-Assets::add_css(array('bootstrap.min.css', 'bootstrap-responsive.min.css'));
+//Assets::add_css(array('bootstrap.min.css', 'bootstrap-responsive.min.css'));
 
 Assets::add_js('bootstrap.min.js');
 
--- public/themes/user/header_bar.php
+++ public/themes/user/header_bar.php
@@ -3,9 +3,9 @@
 			<h3 class="an-logo-heading">
 				<a class="an-logo-link" href="#"><img src="<?php echo base_url('assets/images/icon.svg'); ?>" alt="<?php echo $this->settings_lib->item('site.title'); ?> " width="87" height="31"></a>
 			</h3>
-			<button class="an-btn an-btn-icon toggle-button js-toggle-sidebar">
+			<!--<button class="an-btn an-btn-icon toggle-button js-toggle-sidebar">
 				<i class="icon-list"></i>
-			</button>
+			</button>-->
 			<form class="an-form" action="#">
 				<div class="an-search-field topbar">
 				<input class="an-form-control" type="text" placeholder="Search...">
