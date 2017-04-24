c215f74cae947e5b3a6442ece0242bd6bb85c22a
Dinh Gia Bao <baodg@sixthgearstudios.com>
2017-04-24 11:58:38 +0700

    add view, change model

---
+++ application/modules/step/controllers/Step.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+class Step extends Authenticated_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function detail($project_key = null, $action_key = null)
+	{
+		
+	}
+}
