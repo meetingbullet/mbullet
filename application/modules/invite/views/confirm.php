977ae59008db093f5746e127e2cc854b7cb609b0
VietHD <viethd@sixthgearstudios.com>
2017-04-27 14:05:13 +0700

    Change avatar_url to use CI Image library

--- application/modules/invite/views/confirm.php
+++ application/modules/invite/views/confirm.php
@@ -2,16 +2,16 @@
 		<?php echo form_open() ?>
 
 		<div class="row">
-			<div class="col-md-offset-2 col-md-8 col-sm-12">
+			<div class="col-md-offset-2 col-md-8 col-sm-12 col-lg-6 col-lg-offset-3">
 				<div class="an-single-component with-shadow">
 					<div class="an-component-header">
 						<h6><?php e(lang('iv_invitation')) ?></h6>
 					</div>
 					<div class="an-component-body">
 						<div class="an-helper-block text-center">
-									<div class='avatar' style='background-image:url("<?php echo avatar_url($invitation->inviter_avatar, $invitation->inviter_email) ?>")'></div>
+									<div class='avatar' style='background-image:url("<?php echo avatar_url($invitation->inviter_avatar, $invitation->inviter_email, 50) ?>")'></div>
 									<i class='ion-ios-plus-empty big-plus'></i>
-									<div class='avatar' style='background-image:url("<?php echo avatar_url($invitation->my_avatar, $invitation->my_email) ?>")'></div>
+									<div class='avatar' style='background-image:url("<?php echo avatar_url($invitation->my_avatar, $invitation->my_email, 50) ?>")'></div>
 									<h4><?php echo sprintf(lang('iv_inviting_introduction'), $invitation->inviter_name, $invitation->organization_name)?></h4>
 									<button type="submit" name="accept" class="an-btn an-btn-success"><?php e(lang('iv_accept'))?></button>
 									<button type="submit" name="decline" class="an-btn an-btn-danger-transparent"><?php e(lang('iv_decline'))?></button>
