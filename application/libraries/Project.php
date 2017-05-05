5b085da51648c742304c8ec6d990a57f51baeae1
VietHD <viethd@sixthgearstudios.com>
2017-05-05 19:22:44 +0700

    Update step

--- application/libraries/Project.php
+++ application/libraries/Project.php
@@ -37,4 +37,46 @@ class Project
 
 		return $parent_key . '-' . (empty($last_key) ? 1 : ($last_key + 1));
 	}
+
+	public function has_permission($object, $object_key, $permission_name)
+	{
+		//get project_id
+		switch ($object) {
+			case 'project':
+				$query = $this->ci->db->select('project_id')->from('projects')->where('project_key', $object_key)->get();
+				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
+				else return false;
+				break;
+			case 'action':
+				$query = $this->ci->db->select('project_id')->from('actions')->where('action_key', $object_key)->get();
+				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
+				else return false;
+				break;
+			case 'step':
+				$query = $this->ci->db->select('a.project_id')->from('actions a')->join('steps s', 'a.action_id = s.action_id')->where('s.step_key', $object_key)->get();
+				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
+				else return false;
+				break;
+			case 'task':
+				$query = $this->ci->db->select('a.project_id')->from('actions a')->join('steps s', 'a.action_id = s.action_id')->join('tasks t', 't.step_id = s.step_id')->where('t.task_key', $object_key)->get();
+				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
+				else return false;
+				break;
+			default:
+				return false;
+		}
+
+		//check logged user is project owner ?
+		$query = $this->ci->db->select('project_id')->where('project_id', $project_id)->where('owner_id', $this->ci->auth->user()->user_id)->get('projects');
+		if ($query->num_rows() > 0) return true;
+
+		//check logged user is project member ?
+		$query = $this->ci->db->select('project_id')->where('project_id', $project_id)->where('user_id', $this->ci->auth->user()->user_id)->get('project_members');
+		if ($query->num_rows() > 0) return true;
+
+		//check logged user has permission ?
+		if (has_permission($permission_name)) return true;
+
+		return false;
+	}
 }
--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -8,6 +8,7 @@ class Dashboard extends Authenticated_Controller
 		$this->lang->load('step/step');
 		$this->load->model('projects/project_model');
 		$this->load->model('step/step_model');
+		$this->load->model('step/step_member_model');
 
 		Assets::add_module_js('dashboard', 'dashboard.js');
 		Assets::add_module_css('step', 'step.css');
@@ -24,12 +25,22 @@ class Dashboard extends Authenticated_Controller
 
 
 		$my_steps = $this->step_model->select('steps.*,  CONCAT(u.first_name, " ", u.last_name) as owner_name')
-									->join('step_members sm', 'sm.step_id = steps.step_id AND sm.user_id = ' . $this->current_user->user_id ,'LEFT')
 									->join('users u', 'u.user_id = steps.owner_id')
 									->where('(status = "ready" OR status = "inprogress")', null, false)
+									->where('owner_id', $this->current_user->user_id)
+									->find_all();
+		$my_steps = $my_steps && count($my_steps) > 0 ? $my_steps : [];
+
+		$member_steps = $this->step_member_model->select('s.*,  CONCAT(u.first_name, " ", u.last_name) as owner_name')
+									->join('steps s', 's.step_id = step_members.step_id')
+									->join('users u', 'u.user_id = step_members.user_id')
+									->where('(status = "ready" OR status = "inprogress")', null, false)
+									->where('step_members.user_id', $this->current_user->user_id)
 									->find_all();
 
-		Template::set('my_steps', $my_steps && count($my_steps) > 0 ? $my_steps : []);
+		$member_steps = $member_steps && count($member_steps) > 0 ? $member_steps : [];
+
+		Template::set('my_steps', array_merge($my_steps, $member_steps));
 
 		Assets::add_js($this->load->view('index_js', ['now' => gmdate('Y-m-d H:i:s')], true), 'inline');
 		Template::set('current_user', $this->current_user);
