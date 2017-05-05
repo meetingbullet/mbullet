622db455edc1255185c623c3240261e8a3b87fe9
Le Sy Dat <datls@sixthgearstudios.com>
2017-05-05 20:41:07 +0700

    Lib project: - Add get_object_id - Function has_permission: Change from object_key to object_id

--- application/libraries/Project.php
+++ application/libraries/Project.php
@@ -9,6 +9,13 @@ class Project
 		$this->ci =& get_instance();
 	}
 
+	/**
+	 * Get next key for Project, Action, Step, Task
+	 *
+	 * @param string $parent_key
+	 * @param string $table
+	 * @return string
+	 */
 	public function get_next_key($parent_key, $table = "")
 	{
 		if (empty($table)) {
@@ -28,37 +35,78 @@ class Project
 			if (count($keys) == 3) {
 				$table = 'tasks';
 			}
+		} elseif ($table != 'actions' && $table != 'steps' && $table != 'tasks') {
+			return false;
 		}
 
-		$last_key = $this->ci->db->select('MAX(CAST(REPLACE(`' . rtrim($table, 's') . '_key`, \'' . $parent_key . '-' . '\', \'\') AS UNSIGNED)) AS `last_key`', false)
+		
+		$query = $this->ci->db->select('MAX(CAST(REPLACE(`' . rtrim($table, 's') . '_key`, \'' . $parent_key . '-' . '\', \'\') AS UNSIGNED)) AS `last_key`', false)
 								->like('`' . rtrim($table, 's') . '_key`', $parent_key . '-', 'after')
-								->get($table)
-								->row()->last_key;
-
-		return $parent_key . '-' . (empty($last_key) ? 1 : ($last_key + 1));
+								->get($table);
+		if ($query->num_rows() > 0) {
+			$last_key = $query->row()->last_key;
+			return $parent_key . '-' . (empty($last_key) ? 1 : ($last_key + 1));
+		} else {
+			return false;
+		}
 	}
 
-	public function has_permission($object, $object_key, $permission_name)
+	/**
+	 * Get object ID from object key (Organization checked)
+	 *
+	 * @param string $object_type
+	 * @param string $object_key
+	 * @return int if found, otherwise return false
+	 */
+	public function get_object_id($object_type, $object_key)
 	{
-		//get project_id
 		switch ($object) {
 			case 'project':
-				$query = $this->ci->db->select('project_id')->from('projects')->where('project_key', $object_key)->get();
+				$query = $this->ci->db->select('project_id p')->from('projects')->where('project_key', $object_key)->where('organization_id', $this->ci->auth->current_organization_id)->get();
+				if ($query->num_rows() > 0) return $query->row()->project_id;
+				else return false;
+				break;
+			case 'action':
+				$query = $this->ci->db->select('a.action_id')->from('actions a')->join('project p', 'p.project_id = a.project_id')->where('a.action_key', $object_key)->where('p.organization_id', $this->ci->auth->current_organization_id)->get();
+				if ($query->num_rows() > 0) return $query->row()->action_id;
+				else return false;
+				break;
+			case 'step':
+				$query = $this->ci->db->select('s.step_id')->from('steps s')->join('action a', 'a.action_id = s.action_id')->join('projects p', 'p.project_id = a.project_id')->where('s.step_key', $object_key)->where('p.organization_id', $this->ci->auth->current_organization_id)->get();
+				if ($query->num_rows() > 0) return $query->row()->step_id;
+				else return false;
+				break;
+			case 'task':
+				$query = $this->ci->db->select('t.task_id')->from('tasks t')->join('steps s', 's.step_id = t.step_id')->join('action a', 'a.action_id = s.action_id')->join('projects p', 'p.project_id = a.project_id')->where('t.task_key', $object_key)->where('p.organization_id', $this->ci->auth->current_organization_id)->get();
+				if ($query->num_rows() > 0) return $query->row()->task_id;
+				else return false;
+				break;
+			default:
+				return false;
+		}
+	}
+
+	public function has_permission($object_type, $object_id, $permission_name)
+	{
+		//get project_id
+		switch ($object_type) {
+			case 'project':
+				$query = $this->ci->db->select('project_id')->from('projects')->where('project_id', $object_id)->get();
 				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
 				else return false;
 				break;
 			case 'action':
-				$query = $this->ci->db->select('project_id')->from('actions')->where('action_key', $object_key)->get();
+				$query = $this->ci->db->select('project_id')->from('actions')->where('action_id', $object_id)->get();
 				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
 				else return false;
 				break;
 			case 'step':
-				$query = $this->ci->db->select('a.project_id')->from('actions a')->join('steps s', 'a.action_id = s.action_id')->where('s.step_key', $object_key)->get();
+				$query = $this->ci->db->select('a.project_id')->from('actions a')->join('steps s', 'a.action_id = s.action_id')->where('s.step_id', $object_id)->get();
 				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
 				else return false;
 				break;
 			case 'task':
-				$query = $this->ci->db->select('a.project_id')->from('actions a')->join('steps s', 'a.action_id = s.action_id')->join('tasks t', 't.step_id = s.step_id')->where('t.task_key', $object_key)->get();
+				$query = $this->ci->db->select('a.project_id')->from('actions a')->join('steps s', 'a.action_id = s.action_id')->join('tasks t', 't.step_id = s.step_id')->where('t.task_id', $object_id)->get();
 				if ($query->num_rows() > 0) $project_id = $query->row()->project_id;
 				else return false;
 				break;
