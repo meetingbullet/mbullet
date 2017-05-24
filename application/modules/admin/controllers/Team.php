cd0c8233d5f8478f5a38b3fb974d745837c03fa2
Dinh Gia Bao <baodg@sixthgearstudios.com>
2017-05-24 15:39:52 +0700

    Add edit user screen

--- application/modules/admin/controllers/Team.php
+++ application/modules/admin/controllers/Team.php
@@ -11,6 +11,7 @@ class Team extends Authenticated_Controller
 		$this->load->model('invite/user_invite_model');
 		$this->load->model('roles/role_model');
 		$this->load->model('invite/user_to_organizations_model');
+		$this->load->model('organization/organization_model');
 		Assets::add_module_js('admin', 'invite.js');
 		$this->lang->load('project/project');
 		$this->lang->load('admin/admin');
@@ -110,6 +111,9 @@ class Team extends Authenticated_Controller
 		}
 		$users_list['data'] = $users;
 
+		$organization = $this->organization_model->find($this->current_user->current_organization_id);
+
+		Template::set('organization', $organization);
 		Template::set('roles', $roles);
 		Template::set('users_list', $users_list);
 		Template::render();
@@ -144,7 +148,97 @@ class Team extends Authenticated_Controller
 				Template::set_message($message, 'danger');
 			}
 		}
-
 		Template::render();
+	}
+
+	public function edit_user($user_id)
+	{
+		if (! $this->input->is_ajax_request()) {
+			redirect(DEFAULT_LOGIN_LOCATION);
+		}
+
+		if (empty($user_id)) {
+			Template::set('close_modal', 1);
+			Template::set('message_type', 'danger');
+			Template::set('message', lang('ad_tm_user_not_found'));
+		}
+
+		$roles = $this->role_model->get_organization_roles($this->current_user->current_organization_id, 'name, role_id');
+		if (empty($roles)) {
+			Template::set('close_modal', 1);
+			Template::set('message_type', 'danger');
+			Template::set('message', lang('ad_tm_role_not_found'));
+		}
+
+		$temp = [];
+		foreach ($roles as $role) {
+			$temp[$role->role_id] = $role->name;
+		}
+		$roles = $temp;
+		$user = $this->user_model->select('users.*, uto.organization_id, uto.role_id, uto.title, uto.cost_of_time, uto.enabled, CONCAT(first_name, " ", last_name) as full_name')
+								->join('user_to_organizations uto', 'uto.user_id = users.user_id', 'left')
+								->find($user_id);
+		if (empty($user)) {
+			Template::set('close_modal', 1);
+			Template::set('message_type', 'danger');
+			Template::set('message', lang('ad_tm_user_not_found'));
+		}
+
+		if ($this->input->post()) {
+			$this->form_validation->set_rules([
+				[
+					'field' => 'role_id',
+					'label' => 'lang:ad_tm_role',
+					'rules' => 'trim|required|numeric',
+				],
+				[
+					'field' => 'cost_of_time',
+					'label' => 'lang:ad_tm_cost_of_time',
+					'rules' => 'trim|required|numeric',
+				],
+				[
+					'field' => 'title',
+					'label' => 'lang:ad_tm_title',
+					'rules' => 'trim|max_length[255]',
+				],
+				[
+					'field' => 'enabled',
+					'label' => 'lang:ad_tm_enabled',
+					'rules' => 'trim|numeric',
+				]
+			]);
+			if ($this->form_validation->run() !== false) {
+				$data['role_id'] = $this->input->post('role_id');
+				$data['title'] = empty($this->input->post('title')) ? null : $this->input->post('title');
+				$data['cost_of_time'] = $this->input->post('cost_of_time');
+				if (empty($this->input->post('enabled'))) {
+					$data['enabled'] = 0;
+				}
+
+				$updated = $this->user_to_organizations_model->where('organization_id', $this->current_user->current_organization_id)->skip_validation(true)->update($user_id, $data);
+
+				if ($updated) {
+					Template::set('close_modal', 1);
+					Template::set('message_type', 'success');
+					Template::set('message', lang('ad_tm_update_success'));
+				} else {
+					Template::set('close_modal', 0);
+					Template::set('message_type', 'danger');
+					Template::set('message', lang('ad_tm_update_fail'));
+				}
+			} else {
+				Template::set('close_modal', 0);
+				Template::set('message_type', 'danger');
+				Template::set('message', validation_errors());
+			}
+		}
+
+		Template::set('roles', $roles);
+		Template::set('user', $user);
+		if ($this->input->is_ajax_request()) {
+			Template::render('ajax');
+		} else {
+			Template::render();
+		}
 	}
 }
