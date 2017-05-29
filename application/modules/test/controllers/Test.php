c5e3c253d7014f81a23368264bc2a76847995fc9
Dinh Gia Bao <baodg@sixthgearstudios.com>
2017-05-29 17:48:07 +0700

    Add test tool

--- application/modules/test/controllers/Test.php
+++ application/modules/test/controllers/Test.php
@@ -29,4 +29,85 @@ class Test extends Front_Controller
 			echo 'false';die;
 		}
 	}
+
+	public function index()
+	{
+
+		$user = $this->auth->user();
+		$this->config->load('users/google_api');
+
+		require_once APPPATH . 'modules/users/libraries/google-api-client/vendor/autoload.php';
+		$client_id = $this->config->item('client_id');
+		$client_secret = $this->config->item('client_secret');
+
+		try {
+			$client = new Google_Client();
+			$client->setAccessType("offline");
+			$client->setClientId($client_id);
+			$client->setClientSecret($client_secret);
+			$client->refreshToken($user->google_refresh_token);
+			$token = $client->getAccessToken();
+
+			$service = new Google_Service_Calendar($client);
+
+			$calendars = $service->calendarList->listCalendarList();
+			$calendar_list = [];
+			if (! isset($calendars->error)) {
+				while (true) {
+					foreach ($calendars->getItems() as $calendar) {
+						$calendar_list[$calendar->id] = $calendar->summary;
+					}
+					$pageToken = $calendars->getNextPageToken();
+					if ($pageToken) {
+						$calOptParams['pageToken'] = $pageToken;
+						$calendars = $service->calendarList->listCalendarList($calOptParams);
+					} else {
+						break;
+					}
+				}
+			}
+
+			if ($this->input->post()) {
+				$day = trim($this->input->post('no_of_days'));
+				if ($day == '') {
+					$day = 0;
+				}
+				$calendarId = $this->input->post('calendar_id');
+
+				$eventOptParams = array(
+					// 'maxResults' => 10,
+					// 'orderBy' => 'startTime',
+					// 'singleEvents' => TRUE,
+					'timeMin' => date('c', strtotime('now -' . $day . ' days')),
+					'timeMax' => date('c')
+				);
+
+				$events = $service->events->listEvents($calendarId, $eventOptParams);
+				if (! isset($events->error)) {
+					$total_time = 0;
+					while (true) {
+						foreach ($events->getItems() as $event) {
+							$start = strtotime(empty($event->start->dateTime) ? $event->start->date : $event->start->dateTime);
+							$end = strtotime(empty($event->end->dateTime) ? $event->end->date : $event->end->dateTime);
+
+							$total_time += $end - $start;
+						}
+						$pageToken = $events->getNextPageToken();
+						if ($pageToken) {
+							$eventOptParams['pageToken'] = $pageToken;
+							$events = $service->events->listEvents($calendarId, $eventOptParams);
+						} else {
+							break;
+						}
+					}
+				}
+			}
+		} catch (Exception $e) {
+			$total_time = 0;
+		}
+
+		Template::set('total_time', isset($total_time) ? $total_time : null);
+		Template::set('calendar_list', $calendar_list);
+		Template::render();
+	}
 }
