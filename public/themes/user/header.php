b72459b3a76c3680ce1fe64eac5d03bf7571e2e1
VietHD <viethd@gearinc.com>
2017-07-27 14:45:53 +0700

    Add GA

--- public/themes/user/header.php
+++ public/themes/user/header.php
@@ -34,6 +34,29 @@ Assets::add_css(['bootstrap-editable.css', 'fullcalendar.min.css', 'animate.min.
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+
+	<!-- GA -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-101601787-1', 'auto');
+		ga('send', 'pageview');
+
+		function timer06(){ga('send', 'event', 'TimeOnPage', '1', '<?php echo $page_title ?> 6 seconds', { 'nonInteraction': 1 });}
+		function timer61(){ga('send', 'event', 'TimeOnPage', '1', '<?php echo $page_title ?> 61 seconds', { 'nonInteraction': 1 });}
+		function timer301(){ga('send', 'event', 'TimeOnPage', '1', '<?php echo $page_title ?> 301 seconds', { 'nonInteraction': 1 });}
+		function timer601(){ga('send', 'event', 'TimeOnPage', '1', '<?php echo $page_title ?> 601 seconds', { 'nonInteraction': 1 });}
+		ga('send', 'event', 'TimeOnPage', '0', '<?php echo $page_title ?>  Total PVs', { 'nonInteraction': 1 });
+
+		setTimeout(timer06,6000);
+		setTimeout(timer61,61000);
+		setTimeout(timer301,301000);
+		setTimeout(timer601,601000);
+	</script>
+
 </head>
 <body>
 	<div class="main-wrapper">
